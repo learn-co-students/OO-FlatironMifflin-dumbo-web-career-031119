cb41b7ebd8f65d9992067316492d84bba82c8ae6
Daniel Ko <dankomong@gmail.com>
2019-03-18 17:28:02 -0400

    Finish

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,37 @@
 class Employee
 
+  attr_accessor :name, :salary, :manager
+
+  @@employees = []
+
+  def initialize(name, salary, manager)
+    @name = name
+    @salary = salary
+    @manager = manager
+    @@employees << self
+  end
+
+  def self.all
+    @@employees
+  end
+
+  def self.paid_over(salaryNum)
+    self.all.select do |employee|
+      employee.salary > salaryNum
+    end
+  end
+
+  def self.find_by_department(name)
+    employee1 = self.all.find do |employee|
+      employee.manager.department == name
+    end
+    employee1.manager.department
+  end
+
+  def tax_bracket
+    @@employees.select do |employee|
+      self.salary + 1000 >= employee.salary && self.salary - 1000 <= employee.salary
+    end
+  end
+
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,47 @@
 class Manager
 
+  attr_accessor :name, :department, :age
+
+  @@managers = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @@managers << self
+  end
+
+  def self.all
+    @@managers
+  end
+
+  def employees
+    Employee.all.select do |employee|
+      employee.manager == self
+    end
+  end
+
+  def hire_employee(name, salary)
+    employee = Employee.new(name, salary, self)
+  end
+
+  def self.all_departments
+    self.all.map do |manager|
+      manager.department
+    end
+  end
+
+  def self.average_age
+    age_arr = self.all.map do |manager|
+      manager.age
+    end
+
+    sum = 0
+    age_arr.each do |age|
+      sum += age
+    end
+
+    average = sum.to_f/age_arr.length
+  end
+
 end
