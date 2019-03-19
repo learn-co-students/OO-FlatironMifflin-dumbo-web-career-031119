c216186cc36eb4abc24e5d34da7af3a59f8a65c6
Jeff Yu <jwy5140@gmail.com>
2019-03-18 23:46:59 -0400

    v1.1 main features complete

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,42 @@
 class Employee
 
+    attr_accessor :name
+    attr_reader :salary, :manager_name
+
+    @@all = []
+
+    def initialize(name, salary)
+        @name = name
+        @salary = salary
+        @@all << self
+    end
+
+    def manager_name
+        Manager.all.find {|manager|
+            if manager.employees.include?(self)
+                return manager.name
+            end
+        }
+    end
+
+    def self.paid_over(num)
+        Employee.all.select {|employee| employee.salary > num}
+    end
+
+    def self.find_by_department(dep_name)
+        Employee.all.find {|employee| 
+            Manager.all.each {|manager|
+                employee.manager_name === manager.name && manager.department === dep_name
+            }
+        }
+    end
+
+    def tax_bracket
+        Employee.all.select {|employee| employee.salary > (self.salary - 1000) && employee.salary < (self.salary + 1000)}
+    end
+
+    def self.all
+        @@all
+    end
+
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,34 @@
 class Manager
 
+    attr_accessor :name, :department
+    attr_reader :age, :employees
+
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @employees = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def hire_employee(name, salary)
+        @employees << Employee.new(name, salary, self.name)
+    end
+
+    def self.all_departments
+        (Manager.all.collect {|manager| manager.department}).uniq()
+    end
+
+    def self.average_age
+        age = 0
+        (Manager.all.each {|manager| age += manager.age})
+        return age/Manager.all.length
+    end
+
 end
